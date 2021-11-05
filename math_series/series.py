1a144e310d529f0826f60558cee937a4775d2542
AbrarAlzubaidi <abraralzobaide98@gmail.com>
2021-11-06 01:24:36 +0200

    solved

--- math_series/series.py
+++ math_series/series.py
@@ -21,14 +21,21 @@ def series2(n):
         return 1+series2(n-1)
 
 
-def sum_series(n, option1=None, option2=None):
-    if option1 == None and option2 == None:
-        return fibonacci(n)
-    elif option1 == 2 or option2 == 1:
-        return lucas(n)
+def sum_series(n, option1=0, option2=1):
+    # if option1 == 0 and option2 == 1:
+    #     return fibonacci(n)
+    # elif option1 == 2 or option2 == 1:
+    #     return lucas(n)
+    # else:
+    #     return series2(n)
+    if n==0:
+        return option1
+    if n==1:
+        return option2
     else:
-        return series2(n)
+        return sum_series(n-1, option1, option2) +sum_series(n-2,option1,option2)
 
         
+if __name__ == "__main__":
 
-sum_series(5,4,2)
+    print(sum_series(6, 4, 1))
